e75199189b63c4bcef1384cf0b0a9572bb2754e0
Yarchik <yaroslav777007@gmail.com>
2026-09-06 16:42:01 +0500

    Update CalculatorScreen.kt

--- app/src/main/java/com/example/calculator/ui/CalculatorScreen.kt
+++ app/src/main/java/com/example/calculator/ui/CalculatorScreen.kt
@@ -2,15 +2,7 @@ package com.example.calculator.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
